a4540067af193b0dca830c84883f7d3aed5390f6
Christopher Cantrell <topherCantrell@gmail.com>
2020-02-06 15:44:00 -0600

    assembler application

--- src/opcodetools/asm.py
+++ src/opcodetools/asm.py
@@ -1,12 +1,14 @@
 import opcodetools.assembler.assembler_manager
 import opcodetools.cpu.cpu_manager
 
-
+# TODO: this should come from command line
 filename = '../../test/test_assembler/DoubleGap.asm'
 
+# Look for the CPU define if it wasn't given on the command line
 with open(filename) as f:
     lines = f.readlines()
     
+# Get the CPU and Assembler
 cp = None
 for line in lines:
     if line.strip().startswith('._CPU'):
@@ -17,13 +19,18 @@ for line in lines:
         i = line.find('=')
         if i>=0:
             name = line[i+1:].strip()
-            cp = opcodetools.cpu.cpu_manager.get_cpu_by_name(name)
-        
-asm = opcodetools.assembler.assembler_manager.get_assembler_by_name(name)
+            cp = opcodetools.cpu.cpu_manager.get_cpu_by_name(name)        
+            asm = opcodetools.assembler.assembler_manager.get_assembler_by_name(name)
+        break
 
+# Load the lines and strip the code
 lines = asm.load_lines(filename)
 asm.lines = lines
 asm.code = asm.remove_comments_and_blanks(lines)
+
+# Assemble the code
 asm.assemble()
+
+# Write the output
 asm.write_listing('test.lst')
 asm.write_binary('test.bin')
