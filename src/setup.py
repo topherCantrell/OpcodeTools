60b4b50de01d30911624b15ebf3b06360c296bf4
Chris Cantrell <topherCantrell@gmail.com>
2020-02-14 17:58:39 -0600

    updated version

--- src/setup.py
+++ src/setup.py
@@ -2,12 +2,17 @@ import setuptools
 
 # https://dzone.com/articles/executable-package-pip-install
 
+'''
+py setup.py bdist_wheel
+py  -m twine upload dist/*
+'''
+
 with open("../README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name='opcodetools',
-    version='0.1',
+    version='1.0',
     author="Christopher Cantrell",
     author_email="topherCantrell@gmail.com",
     description="Assemblers and Disassemblers",
